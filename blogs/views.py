3b264b6ae80b8185010c0245078f2cd53fcc8153
Marsh <holyfield444@gmail.com>
2026-01-31 09:45:48 +0100

    Did the category template

--- blogs/views.py
+++ blogs/views.py
@@ -1,13 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
-from .models import Blog
+from .models import Blog, Category
 
 # Create your views here.
 
 def posts_by_category(request, category_id):
 
     posts = Blog.objects.filter(category=category_id, status='published').order_by('updated_at')
+        # try:
+        #     category = Category.objects.get(pk=category_id)
+        # except Category.DoesNotExist:
+        #     category = "Unknown Category"
+    category = get_object_or_404(Category, pk=category_id)
+            
     context = {
-        'posts': posts
+        'posts': posts,
+        'category': category
     }
     return render(request, 'category.html', context)
