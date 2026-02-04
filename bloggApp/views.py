bd13ed89aa0a42fbd828c1bf97c900d249b1e999
Marsh <holyfield444@gmail.com>
2026-02-04 12:28:13 +0100

    Registration and login functionality

--- bloggApp/views.py
+++ bloggApp/views.py
@@ -1,7 +1,11 @@
 
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import logout, authenticate, login
+from .forms import RegistrationForm
 from blogs.models import  Blog
 from assignment.models import About 
+from django.contrib import messages, auth
 
 def home(request):
     featured_posts = Blog.objects.filter( is_featured = True, status='published').order_by('-updated_at')
@@ -12,8 +16,6 @@ def home(request):
     except About.DoesNotExist:
         about_info = None
     
-    
-
     context= {
         "featured_posts" : featured_posts,
         "blogs" : blogs,
@@ -22,3 +24,47 @@ def home(request):
     }
  
     return render(request, 'home.html', context)
+
+class StyledRegistrationForm(RegistrationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs.update({"class": "form-control"})
+
+
+
+def register(request):
+    if request.method == "POST":
+        form = StyledRegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+        else:
+            print(form.errors)  # Debugging line to see form errors
+    else:
+        form = StyledRegistrationForm()
+
+    context = {"form": form}
+
+    return render(request, "register.html", context)
+
+
+def login(request):
+
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = auth.authenticate(request, username=username, password=password)
+        if user is not None: # checks if user exists
+            auth.login(request, user)
+            return redirect("home")
+        else:
+            messages.error(request, "Invalid username or password.")
+            return redirect("login")
+        
+    return render(request, "login.html")
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect("home")   # or "login"
